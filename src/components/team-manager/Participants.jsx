75fb8281365444ff8bd5812d92771dc08b6458ff
rinshadkl <riinshad18@gmail.com>
2025-09-22 23:32:32 +0630

    fix: Update import statements to include file extensions for data modules

--- src/components/team-manager/Participants.jsx
+++ src/components/team-manager/Participants.jsx
@@ -8,8 +8,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
-import notSelectedParticipants from '@/data/notselected'
-import wishListData from '@/data/wishlist'
+import notSelectedParticipants from '@/data/notselected.js'
+import wishListData from '@/data/wishlist.js'
 
 const Participants = () => {
   const [participantsList] = useState(notSelectedParticipants)
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react'
-import roleplayers from '@/data/roleplayers'
+import roleplayers from '@/data/roleplayers.js'
 
 const AuthContext = createContext(null)
 
